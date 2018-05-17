86b928b9f513001752a537607d61c6ab3ba0636c
luanrsilva <luan.silva@ccc.ufcg.edu.br>
2018-05-17 17:29:58 -0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent} from './heroes/heroes.component';
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {
+
+  const routes: Routes = [
+    {path: 'heroes', component: HeroesComponent}
+  ];
+
+}
